b7aa7f3cb37c150c89f46f64f0ba28c01e3549e4
ahmirdev-28 <devezin_ahmir@students.columbusstate.edu>
2026-02-02 23:55:27 -0500

    Add files via upload

---
+++ First.java
@@ -0,0 +1,21 @@
+
+/**
+ * //Ahmir Devezin
+ *
+ * //Asignment: First Program
+ * 1/22/26
+ */
+public class First
+{
+    public static void main(String[] args)
+    {
+    //Printed words being produced
+    System.out.println("Hola mundo");
+    System.out.println("Job Title: Infromation Security Analysts");
+    System.out.println("Why: Good Technolocial Advancements");
+    System.out.println("Median Salary: $124,000");
+
+    }
+}
+    
+    
