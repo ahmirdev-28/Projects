3c4d4c346156e24f075cb3f901333be3fbb8a2e9
ahmirdev-28 <devezin_ahmir@students.columbusstate.edu>
2026-02-25 22:31:43 -0500

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -1,39 +1,10 @@
-import java.util.Scanner;
-/**
- * 
- *
- * @Ahmir Devezin
- * @2/25/26
- */
-public class Runner
-{
+public class Runner {
     public static void main(String[] args) {
 
-    Scanner scnr = new Scanner(System.in);
+        Tests t1 = new Tests();
 
-    // First Pet - default constructor
-    Pet pet1 = new Pet();
-    System.out.println(pet1);
+        t1.getAverage();
 
-    // Second Pet - custom constructor
-    Pet pet2 = new Pet("Dog", "Buster", 11);
-    System.out.println(pet2);
-
-    // Third Pet - user input
-    System.out.println("Enter animal type:");
-    String type = scnr.nextLine();
-
-    System.out.println("Enter animal name:");
-    String name = scnr.nextLine();
-
-    System.out.println("Enter animal age:");
-    int age = scnr.nextInt();
-
-    Pet pet3 = new Pet(type, name, age);
-
-    System.out.println();
-    System.out.println(pet3);
-
-    scnr.close();
-}
+        System.out.println(t1);
+    }
 }
--- Tests.java
+++ Tests.java
@@ -1,24 +1,47 @@
+import java.util.Scanner;
 
 /**
  * 
  *
  * @Ahmir Devezub
- * @2/2/26
+ * @2/25/26
  */
 public class Tests
 {
-    public static void main(String[] args){
-        //test scores
-        double testScore1 = 88.2;
-        double testScore2 = 78.9;
-        double testScore3 = 97.6;
+   private int count;
+   private double average;
+   
+public Tests(){
+    //private fields
+    count = 0;
+    average = 0.0;
+}
+
+public void getAverage(){
+    Scanner scnr = new Scanner(System.in);
+    
+    double sum = 0.0;
+    int localCount = 0;
+    
+    System.out.println("Enter test scores (press -1 to quit):");
+    
+    double score = scnr.nextDouble();
+    
+    while(score != -1) {
+        sum += score;
+        localCount++;
         
-        //using the average
-        double average = (testScore1 + testScore2 + testScore3) / 3;
-        
-          System.out.println("Test score 1:   " + testScore1);
-        System.out.println("Test score 2:  " + testScore2);
-        System.out.println("Test score 3:  " + testScore3);
-        System.out.printf("The average of 3 test scores is:  %.2f%n", average);
+        System.out.println("Enter test scores (press -1 to quit):");
+        score = scnr.nextDouble();
     }
+    
+    count = localCount;
+    average = sum / localCount;
+}
+
+public String toString() {
+    return "The average of the " + count +
+    " scores entered is " + 
+    String.format("%2.f", average) + ".";
+}
 }
