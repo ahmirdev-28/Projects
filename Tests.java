7c24c98414d1865af06c11d71985c2e6bbd31d58
ahmirdev-28 <devezin_ahmir@students.columbusstate.edu>
2026-03-10 12:04:24 -0400

    Add files via upload

--- Tests.java
+++ Tests.java
@@ -8,40 +8,49 @@ import java.util.Scanner;
  */
 public class Tests
 {
-   private int count;
-   private double average;
-   
-public Tests(){
-    //private fields
-    count = 0;
-    average = 0.0;
-}
-
-public void getAverage(){
-    Scanner scnr = new Scanner(System.in);
-    
-    double sum = 0.0;
-    int localCount = 0;
-    
-    System.out.println("Enter test scores (press -1 to quit):");
-    
-    double score = scnr.nextDouble();
-    
-    while(score != -1) {
-        sum += score;
-        localCount++;
-        
-        System.out.println("Enter test scores (press -1 to quit):");
-        score = scnr.nextDouble();
-    }
-    
-    count = localCount;
-    average = sum / localCount;
-}
-
-public String toString() {
-    return "The average of the " + count +
-    " scores entered is " + 
-    String.format("%2.f", average) + ".";
-}
-}
+    // private fields
+  private int numScores;
+  private double average;
+  
+  //constructors
+  public Tests(){
+      numScores = 0;
+      average = 0.0;
+  }
+  
+  //acessors
+  public int getNumScores(){
+      return numScores;
+  }
+  
+  public void getAverage(){
+      
+      Scanner scnr = new Scanner(System.in);
+      
+      double sum = 0.0;
+      int localCount = 0;
+      
+      System.out.println("Enter test scores (press -1 to quit):");
+      double score = scnr.nextDouble();
+      
+      while (score != -1){
+          
+          sum +=score;
+          localCount++;
+          
+          System.out.println("Enter test scores (press -1 to quit:");
+          score = scnr.nextDouble();
+          
+         
+          
+          
+      }
+       numScores = localCount;
+          average = sum / localCount;
+        }
+        public String toString(){
+              return "The average of the " + numScores +
+              " scores entered is " + 
+              String.format("%.2f", average) + ".";
+      }
+  }
