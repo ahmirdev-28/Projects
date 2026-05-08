7b5a898413549301c7c22a8479cb5423f36ff068
ahmirdev-28 <devezin_ahmir@students.columbusstate.edu>
2026-05-07 22:45:50 -0400

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -1,48 +1,16 @@
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class Runner {
-
     public static void main(String[] args) {
 
-        int questions = 0;
-        int missed = 0;
+        Scanner input = new Scanner(System.in);
 
-        try {
+        System.out.print("Enter number of disks: ");
+        int n = input.nextInt();
 
-            String input;
+        System.out.println("\nTowers of Hanoi Steps:\n");
+        Towers.move(n, 'A', 'C', 'B');
 
-            input = JOptionPane.showInputDialog("How many questions are on the exam?");
-            questions = Integer.parseInt(input);
-
-            input = JOptionPane.showInputDialog("How many questions did the student miss?");
-            missed = Integer.parseInt(input);
-
-            if (questions <= 0) {
-                throw new ArithmeticException("Questions must be more than 0.");
-            }
-
-            if (missed < 0 || missed > questions) {
-                throw new IllegalArgumentException("Missed questions can't be negative or more than total.");
-            }
-
-            Exam exam = new Exam(questions, missed);
-
-            String message = "Each question is worth " + exam.getPointsEach() + " points.\n";
-            message += "Score: " + exam.getScore() + "\n";
-            message += "Grade: " + exam.getGrade();
-
-            JOptionPane.showMessageDialog(null, message);
-
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Please enter numbers only.");
-
-        } catch (ArithmeticException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage());
-
-        } catch (IllegalArgumentException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage());
-        }
-
-        System.exit(0);
+        input.close();
     }
 }
